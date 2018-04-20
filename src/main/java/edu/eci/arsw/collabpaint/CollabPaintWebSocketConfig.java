baafaee07d3d09765739747283561db441dde3bd
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-04-19 22:01:35 -0500

    a

--- src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
@@ -42,7 +42,7 @@ public class CollabPaintWebSocketConfig extends AbstractWebSocketMessageBrokerCo
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         //System.out.println(userBucketPath);
-        config.enableStompBrokerRelay("/topic").setRelayHost("127.0.0.1").setRelayPort(61613);
+        config.enableStompBrokerRelay("/topic").setRelayHost("127.0.0.1").setRelayPort(8080);
         config.setApplicationDestinationPrefixes("/app");
     }
 
