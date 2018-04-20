9332bbf1b14a4a0e7198fff8f4122eb42a1abe60
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-04-19 20:31:36 -0500

    AS

--- src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
@@ -21,6 +21,7 @@ package edu.eci.arsw.collabpaint;
  * @author hcadavid
  */
 import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.AntPathMatcher;
@@ -32,15 +33,19 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class CollabPaintWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
+    @Value("${userBucket.path}")
+    private String userBucketPath;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        System.out.println(userBucketPath);
         config.enableStompBrokerRelay("/topic").setRelayHost("127.0.0.1").setRelayPort(61613);
-        config.setApplicationDestinationPrefixes("/app");        
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();  
-        
+        registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
+
     }
 }
