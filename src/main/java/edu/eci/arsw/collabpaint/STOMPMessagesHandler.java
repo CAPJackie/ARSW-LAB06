325b33df03d8d6ff4b6cf0199751a4dd4ea683c9
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-04-22 17:03:42 -0500

    REDIS configuration added

--- src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
+++ src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
@@ -1,38 +1,55 @@
 package edu.eci.arsw.collabpaint;
 
-
 import edu.eci.arsw.collabpaint.model.Point;
+import edu.eci.arsw.collabpaint.util.JedisUtil;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
 
 @Controller
 public class STOMPMessagesHandler {
-	
-	@Autowired
-	SimpMessagingTemplate msgt;
+
+    @Autowired
+    SimpMessagingTemplate msgt;
+
+    ConcurrentHashMap<String, ArrayList<Point>> polygonPoints = new ConcurrentHashMap();
+
+    @MessageMapping("/newpoint.{numdibujo}")
+    public void handlePointEvent(Point pt, @DestinationVariable String numdibujo) throws Exception {
         
-        ConcurrentHashMap<String, ArrayList<Point>> polygonPoints = new ConcurrentHashMap();
-    
-	@MessageMapping("/newpoint.{numdibujo}")    
-	public void handlePointEvent(Point pt,@DestinationVariable String numdibujo) throws Exception {
-		System.out.println("Nuevo punto recibido en el servidor!:"+pt);
-		if(polygonPoints.containsKey(numdibujo)){
-                    polygonPoints.get(numdibujo).add(pt);
-                    if(polygonPoints.get(numdibujo).size() > 3){
-                        msgt.convertAndSend("/topic/newpolygon."+numdibujo, polygonPoints.get(numdibujo));
-                        polygonPoints.clear();
-                    }
-                }
-                else{
-                    ArrayList<Point> puntos = new ArrayList();
-                    puntos.add(pt);
-                    polygonPoints.put(numdibujo, puntos);
-                }
-                //msgt.convertAndSend("/topic/newpoint."+numdibujo, pt);
-	}
+        Jedis jedis = JedisUtil.getPool().getResource();
+        
+        jedis.watch("X","Y");
+        jedis.rpush("X", String.valueOf(pt.getX()));
+        jedis.rpush("Y", String.valueOf(pt.getY()));
+        Transaction tx = jedis.multi();
+        
+        List<Object> res=tx.exec();
+        
+        System.out.println("TAMANO: "+res.size());
+
+
+        System.out.println("Nuevo punto recibido en el servidor!:" + pt);
+        if (polygonPoints.containsKey(numdibujo)) {
+            polygonPoints.get(numdibujo).add(pt);
+            if (polygonPoints.get(numdibujo).size() > 3) {
+                msgt.convertAndSend("/topic/newpolygon." + numdibujo, polygonPoints.get(numdibujo));
+                polygonPoints.clear();
+            }
+        } else {
+            ArrayList<Point> puntos = new ArrayList();
+            puntos.add(pt);
+            polygonPoints.put(numdibujo, puntos);
+        }
+        
+        jedis.close();
+        //msgt.convertAndSend("/topic/newpoint."+numdibujo, pt);
+    }
 }
