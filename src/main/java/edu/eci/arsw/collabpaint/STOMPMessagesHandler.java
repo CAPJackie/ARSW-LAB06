2f140e36d3138b4b5ed17dffdce4b2d06b702f99
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-04-25 21:10:26 -0500

    asd

--- src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
+++ src/main/java/edu/eci/arsw/collabpaint/STOMPMessagesHandler.java
@@ -13,6 +13,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Response;
 import redis.clients.jedis.Transaction;
 
 @Controller
@@ -25,31 +26,38 @@ public class STOMPMessagesHandler {
 
     @MessageMapping("/newpoint.{numdibujo}")
     public void handlePointEvent(Point pt, @DestinationVariable String numdibujo) throws Exception {
-            Jedis jedis = JedisUtil.getPool().getResource();
-            jedis.getClient().setTimeoutInfinite();  
-            
-            
-            jedis.watch("X", "Y");
-            Transaction tx = jedis.multi();
-            tx.rpush("X", String.valueOf(pt.getX()));
-            tx.rpush("Y", String.valueOf(pt.getY()));
-            List<Object> res = tx.exec();
+        Jedis jedis = JedisUtil.getPool().getResource();
+        jedis.getClient().setTimeoutInfinite();
 
-            
-            System.out.println(jedis.lrange("Y", 0, -1));
-            //System.out.println(jedis.llen("Y"));
-            
-            //Set<String> smembers = jedis.smembers("Y");
-            
-            //smembers.forEach(System.out::println);
-            
-            //System.out.println("TAMANO: " + jedis.smembers("Y"));
-            
-            System.out.println("Nuevo punto recibido en el servidor!:" + pt);
-            
-            jedis.close();
+        jedis.watch("X", "Y");
+        Transaction tx = jedis.multi();
+        tx.rpush("X", String.valueOf(pt.getX()));
+        tx.rpush("Y", String.valueOf(pt.getY()));
+        List<Object> res = tx.exec();
 
-           
+        System.out.println(jedis.lrange("Y", 0, -1));
+
+        System.out.println("Nuevo punto recibido en el servidor!:" + pt);
+
+        String luaScript = "local xVal,yVal; \n"
+                + "if (redis.call('LLEN','X')==4) then \n"
+                + "	xVal=redis.call('LRANGE','X',0,-1);\n"
+                + "	yVal=redis.call('LRANGE','Y',0,-1);\n"
+                + "	redis.call('DEL','X');\n"
+                + "	redis.call('DEL','Y');\n"
+                + "	return {xVal,yVal};\n"
+                + "else\n"
+                + "	return {};\n"
+                + "end";
+
+        Response<Object> luares = tx.eval(luaScript.getBytes(), 0, "0".getBytes());
+
+        List<Object> resp = tx.exec();
+
+        if (((ArrayList) luares.get()).size() == 2) {
+            System.out.println(new String((byte[]) ((ArrayList) (((ArrayList) luares.get()).get(0))).get(0)));
+        }
+        jedis.close();
 
         /*if (polygonPoints.containsKey(numdibujo)) {
             polygonPoints.get(numdibujo).add(pt);
